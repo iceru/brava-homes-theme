5efdc730471b1866a4e861e566f7320eb1a93093
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-11-18 11:33:08 +0700

    update homepage styling

--- page-homepage.php
+++ page-homepage.php
@@ -18,7 +18,9 @@ get_header();
                     <img src="<?php bloginfo('template_directory');?>/images/brava-logo-white.png" alt="Brava Homes">
                 </div>
             </div>
-            <div class="bg-video"></div>
+            <div class="cta-video">
+                <img src="<?php bloginfo('template_directory');?>/images/index/cta.gif" alt="Brava Homes">
+            </div>
             <div class="cta-text">
                 The New Definition of Luxury Living Masterfully Designed By Thom Elliot
             </div>
@@ -39,7 +41,7 @@ get_header();
     <div class="row">
         <section class="about-grid">
             <div class="row item">
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-6 item-text">
                     <div class="subtitle">
                         Welcome to
                     </div>
@@ -58,11 +60,16 @@ get_header();
                     <img src="<?php bloginfo('template_directory');?>/images/index/about-grid-1.png" alt="Brava Homes">
                 </div>
             </div>
-            <div class="row item">
+            <div class="row item orange">
                 <div class="col-12 col-lg-6 item-img">
                     <img src="<?php bloginfo('template_directory');?>/images/index/about-grid-2.png" alt="Brava Homes">
                 </div>
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-6 item-text">
+                    <div class="bg">
+                        <img src="<?php bloginfo('template_directory');?>/images/index/bg-grid-orange.png"
+                            alt="Brava Homes">
+
+                    </div>
                     <div class="title">
                         The Home <br>
                         of Dreams
@@ -74,8 +81,8 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="row item">
-                <div class="col-12 col-lg-6">
+            <div class="row item blue">
+                <div class="col-12 col-lg-6 item-text">
                     <div class="title">
                         A New Definition
                         of Luxury
@@ -95,73 +102,89 @@ get_header();
     </div>
 </div>
 
-<div class="container" id="units">
-    <div class="row unit">
-        <div class="col-12 col-lg-6">
-            <img src="<?php bloginfo('template_directory');?>/images/index/himalaya.png" alt="Brava @ Himalaya">
+<div class="units" id="units">
+    <div class="container">
+        <div class="bg">
+            <img src="<?php bloginfo('template_directory');?>/images/index/bg-units.png" alt="Brava Homes">
         </div>
-        <div class="col-12 col-lg-6">
-            <div class="title">
-                BRAVA @ <br> HIMALAYA
+        <div class="row unit">
+            <div class="col-12 col-lg-6 item-img">
+                <img src="<?php bloginfo('template_directory');?>/images/index/himalaya.png" alt="Brava @ Himalaya">
             </div>
-            <div class="text">
-                Brava Homes presents the first enclave of designer homes, Brava Himalaya. The best quality of
-                contemporary living, composed from the best materials and set in a network of modern facilities. Crafted
-                for those high-achievers in life with excellent taste, Brava Himalaya perfectly embodies the achievement
-                of a lifetime.
-            </div>
-            <div class="view">
-                View Units
-                <img src="<?php bloginfo('template_directory');?>/images/chevron-right.png" alt="View">
-            </div>
-        </div>
-    </div>
-    <div class="row unit">
-        <div class="col-12 col-lg-6">
-            <div class="title">
-                BRAVA @ <br> DIPONEGORO
-            </div>
-            <div class="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae purus ut lorem varius rutrum
-                sit amet gravida sem. Nunc lobortis rhoncus ex id dictum. Sed maximus tristique suscipit. Nullam
-                efficitur consectetur libero, ac suscipit diam bibendum.
-            </div>
-            <div class="view">
-                View Units
-                <img src="<?php bloginfo('template_directory');?>/images/chevron-right.png" alt="View">
+            <div class="col-12 col-lg-6 item-text">
+                <div class="title">
+                    BRAVA @ <br> HIMALAYA
+                </div>
+                <div class="text">
+                    Brava Homes presents the first enclave of designer homes, Brava Himalaya. The best quality of
+                    contemporary living, composed from the best materials and set in a network of modern facilities.
+                    Crafted
+                    for those high-achievers in life with excellent taste, Brava Himalaya perfectly embodies the
+                    achievement
+                    of a lifetime.
+                </div>
+                <div class="view">
+                    View Units
+                    <img src="<?php bloginfo('template_directory');?>/images/chevron-right.png" alt="View">
+                </div>
             </div>
         </div>
-        <div class="col-12 col-lg-6">
-            <img src="<?php bloginfo('template_directory');?>/images/index/diponegoro.png" alt="Brava @ Diponegoro">
+        <div class="row unit">
+            <div class="col-12 col-lg-6 item-text">
+                <div class="title">
+                    BRAVA @ <br> DIPONEGORO
+                </div>
+                <div class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae purus ut lorem varius
+                    rutrum
+                    sit amet gravida sem. Nunc lobortis rhoncus ex id dictum. Sed maximus tristique suscipit. Nullam
+                    efficitur consectetur libero, ac suscipit diam bibendum.
+                </div>
+                <div class="view">
+                    View Units
+                    <img src="<?php bloginfo('template_directory');?>/images/chevron-right.png" alt="View">
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 item-img">
+                <img src="<?php bloginfo('template_directory');?>/images/index/diponegoro.png" alt="Brava @ Diponegoro">
+            </div>
         </div>
     </div>
 </div>
 
-<div class="container-fluid">
-    <div class="row" id="amenities">
-        <div class="container">
-            <div class="row amenities">
-                <div class="col-12 col-lg-7">
-                    <div class="title">
-                        Minutes to Amenities
-                    </div>
-                    <div class="text">
-                        Put your mind at ease with a 24/7 security system in place. Aside from that, other amenities
-                        that
-                        include a lifestyle hub, top class educational institutions, and any more are available within
-                        walking distance from the main gates.
-                    </div>
+<div class="amenities" id="amenities">
+    <div class="bg">
+        <img src="<?php bloginfo('template_directory');?>/images/index/amenities.png"
+            alt="Minutes to Amenities Background">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-7">
+                <div class="title">
+                    Minutes to Amenities
+                </div>
+                <div class="text">
+                    Put your mind at ease with a 24/7 security system in place. Aside from that, other amenities
+                    that
+                    include a lifestyle hub, top class educational institutions, and any more are available within
+                    walking distance from the main gates.
                 </div>
             </div>
         </div>
     </div>
-    <div class="row" id="360">
-        <div class="col-12 col-lg-5">
-            <img src="" alt="">
+</div>
+
+<div class="container-fluid" id="tour">
+    <div class="bg">
+        <img src="<?php bloginfo('template_directory');?>/images/index/bg-360-tour.png" alt="360 Tour Background">
+    </div>
+    <div class="row">
+        <div class="col-12 col-lg-5 tour-img">
+            <img src="<?php bloginfo('template_directory');?>/images/index/360-tour.png" alt="360 Tour">
         </div>
-        <div class="col-12 col-lg-7">
+        <div class="col-12 col-lg-7 tour-text">
             <div class="title">
-                TAKE A 360
+                TAKE A 360 <br>
                 TOUR OF BRAVA
             </div>
             <button type="button" class="btn btn-primary">
