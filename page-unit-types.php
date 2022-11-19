1788b7e119ef19a58fbf0036910259021e2d1e08
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-11-19 14:32:00 +0700

    update unit types

--- page-unit-types.php
+++ page-unit-types.php
@@ -39,16 +39,55 @@ get_header();
     <div class="bg">
         <img src="<?php bloginfo('template_directory');?>/images/units/bg-subsection.png" alt="Background">
     </div>
-    <div class="title">
-        A Private Enclave
-    </div>
-    <div class="text">
-        Be the proud owner of this premium cluster in a very artistically done private enclave with an impressive design
-        from Tom Elliot and high-quality material selections that compose the state-of-the-art Brava Homes
+    <div class="content">
+        <div class="title">
+            A Private Enclave
+        </div>
+        <div class="text">
+            Be the proud owner of this premium cluster in a very artistically done private enclave with an impressive
+            design
+            from Tom Elliot and high-quality material selections that compose the state-of-the-art Brava Homes
+        </div>
     </div>
 </div>
 <div class="image">
-    <img src="<?php bloginfo('template_directory');?>/images/units/units-image.png" alt="Brava Homes">
+    <img class="w-100" src="<?php bloginfo('template_directory');?>/images/units/units-image.png" alt="Brava Homes">
+</div>
+<div class="master">
+    <div class="bg">
+        <img src="<?php bloginfo('template_directory');?>/images/units/bg-master.png" alt="Background">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="subtitle">
+                    Variety of Flexible Room Arrangement at
+                </div>
+                <div class="title">
+                    The Master Suite
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="text">
+                    The unmatched comfort of Brava Homes Master Suite is beyond compare. Bringing you the ultimate form
+                    of
+                    comfort in a combination of versatile design and flawless materials. The Master Suite also comes
+                    with an
+                    en suite bathroom, a walk-in closet, a cozy corner office, and a balcony with glass railing.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="siteplan">
+    <div class="container">
+        <div class="title">
+            Siteplan
+        </div>
+    </div>
+    <div class="img">
+        <img src="<?php bloginfo('template_directory');?>/images/units/siteplan.png" alt="Siteplan">
+    </div>
 </div>
 <?php
 get_footer();
