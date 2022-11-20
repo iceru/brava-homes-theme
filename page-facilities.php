15a9eafb4934119fcdc5037761c7adf30474cc7e
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-11-20 19:48:09 +0700

    update facilities

--- page-facilities.php
+++ page-facilities.php
@@ -10,17 +10,28 @@ get_header();
 <div class="masterplan">
     <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/masterplan.png" alt="Masterplan">
 </div>
-<div class="lippo">
+<div class="lippo position-relative">
+    <div class="bg">
+        <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/lippo-bg.png"
+            alt="Background">
+    </div>
     <div class="container">
-        <div class="row">
-            <div class="col-12 col-lg-7">
-                <div class="title">
-                    At The Heart of Lippo Village Township
-                </div>
-                <div class="text">
-                    Located at the center of Lippo Village Central, an established township with comprehensive
-                    facilities and amenities. Brava Homes connects you to our city’s primary education, lifestyle,
-                    entertainment venues, and health care enclaves.
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-7 item-text">
+                <div class="content">
+                    <div class="bg-border">
+                        <img class="w-100"
+                            src="<?php bloginfo('template_directory');?>/images/facilities/lippo-border.png"
+                            alt="Background">
+                    </div>
+                    <div class="title">
+                        At The Heart of Lippo Village Township
+                    </div>
+                    <div class="text">
+                        Located at the center of Lippo Village Central, an established township with comprehensive
+                        facilities and amenities. Brava Homes connects you to our city’s primary education, lifestyle,
+                        entertainment venues, and health care enclaves.
+                    </div>
                 </div>
             </div>
             <div class="col-12 col-lg-5">
@@ -34,64 +45,88 @@ get_header();
     <div class="bg">
         <img class="w-100" src="<?php bloginfo('template_directory');?>/images/units/smart-bg.png" alt="Background">
     </div>
-    <div class="img">
-        <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/map.png"
-            alt="Brava Homes Map">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="img">
+                    <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/map.png"
+                        alt="Brava Homes Map">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="places">
     <div class="row item">
-        <div class="col-12 col-lg-6">
-            <div class="title">
-                Indulge in <br>
-                Your Senses
+        <div class="col-12 col-lg-6 item-text orange">
+            <div class="bg">
+                <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-bg.png"
+                    alt="Background">
             </div>
-            <div class="text">
-                Enjoy your leisurley activities around the Lippo Village Township
-                <ul>
-                    <li>Hive @ Himalaya</li>
-                    <li>Citywalk Elvee</li>
-                    <li>Supermal Karawaci</li>
-                    <li>Maxx Box</li>
-                </ul>
+            <div class="content">
+                <div class="title">
+                    Indulge in <br>
+                    Your Senses
+                </div>
+                <div class="text">
+                    Enjoy your leisurley activities around the Lippo Village Township
+                    <ul>
+                        <li>Hive @ Himalaya</li>
+                        <li>Citywalk Elvee</li>
+                        <li>Supermal Karawaci</li>
+                        <li>Maxx Box</li>
+                    </ul>
+                </div>
             </div>
         </div>
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-lg-6 item-img">
             <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-1.png"
                 alt="Activities">
         </div>
     </div>
     <div class="row item">
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-lg-6 item-img">
             <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-2.png"
                 alt="Education">
         </div>
-        <div class="col-12 col-lg-6">
-            <div class="title">
-                World Class <br> Education
+        <div class="col-12 col-lg-6 item-text blue">
+            <div class="bg">
+                <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-bg.png"
+                    alt="Background">
             </div>
-            <div class="text">
-                Primely located at the center of quality education for your loved ones.
-                <ul>
-                    <li>Sekolah Pelita Harapan</li>
-                    <li>Sekolah Dian Harapan</li>
-                    <li>Universitas Pelita Harapan</li>
-                </ul>
+            <div class="content">
+                <div class="title">
+                    World Class <br> Education
+                </div>
+                <div class="text">
+                    Primely located at the center of quality education for your loved ones.
+                    <ul>
+                        <li>Sekolah Pelita Harapan</li>
+                        <li>Sekolah Dian Harapan</li>
+                        <li>Universitas Pelita Harapan</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
     <div class="row item">
-        <div class="col-12 col-lg-6">
-            <div class="title">
-                10 Minutes Away to
-                Soetta Airport
+        <div class="col-12 col-lg-6 item-text orange">
+            <div class="bg">
+                <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-bg.png"
+                    alt="Background">
             </div>
-            <div class="text">
-                Never be late for your flights again and enjoy the convenience of being in close proximity to the
-                airport.
+            <div class="content">
+                <div class="title">
+                    10 Minutes Away to
+                    Soetta Airport
+                </div>
+                <div class="text">
+                    Never be late for your flights again and enjoy the convenience of being in close proximity to the
+                    airport.
+                </div>
             </div>
         </div>
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-lg-6 item-img">
             <img class="w-100" src="<?php bloginfo('template_directory');?>/images/facilities/places-3.png"
                 alt="Airport">
         </div>
