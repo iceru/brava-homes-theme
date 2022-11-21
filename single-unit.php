17d1cebddcd69a34df1b3e8bfbdbf55910f1f267
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-11-21 18:20:35 +0700

    initialize single unit

---
+++ single-unit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Blog Index
+ * Description: The template for displaying the Blog index /blog.
+ *
+ */
+
+get_header(); 
+?>
+<div class="unit-header">
+
+</div>
+<?php
+get_footer();
