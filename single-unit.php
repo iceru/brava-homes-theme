3a5a7236ca43c978564daaa96d554860a208f0ae
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-11-22 23:58:43 +0700

    finalize unit

--- single-unit.php
+++ single-unit.php
@@ -5,8 +5,11 @@
  *
  */
 
-get_header(); 
+get_header();
 
+for ($x = 0; $x <= 5; $x++) {
+    ${'type'.$x} = get_field('types_'.$x);
+}
 ?>
 <div class="unit-header">
     <div class="bg">
@@ -32,62 +35,141 @@ get_header();
             <div class="col-12 col-lg-8">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <div class="name">Types</div>
+                    <?php for ($x = 1; $x < 6; $x++) {
+                    ?>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="types-1-tab" data-bs-toggle="pill" data-bs-target="#types-1"
-                            type="button" role="tab" aria-controls="types-1" aria-selected="true">Home</button>
+                        <button class="nav-link <?php echo $x === 1 ? 'active' : '' ?>" id="types-<?php echo $x ?>-tab"
+                            data-bs-toggle="pill" data-bs-target="#types-<?php echo $x ?>" type="button" role="tab"
+                            aria-controls="types-<?php echo $x ?>"
+                            aria-selected="true"><?php echo  ${'type'.$x}['title_type'] ?></button>
                     </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
-                            aria-selected="false">Profile</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
-                            aria-selected="false">Contact</button>
-                    </li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="types-1" role="tabpanel" aria-labelledby="types-1-tab"
-                tabindex="0">
+            <?php for ($x = 1; $x < 6; $x++) {
+                    ?>
+            <div class="tab-pane fade show tab-<?php echo $x ?>  <?php echo $x === 1 ? 'active' : '' ?>"
+                id="types-<?php echo $x ?>" role="tabpanel" aria-labelledby="types-<?php echo $x ?>-tab" tabindex="0">
                 <div class="row">
                     <div class="col-12 col-lg-4">
                         <div class="row">
                             <div class="col-12 type">
                                 <div class="subtitle">Type</div>
                                 <div class="title">
-                                    C017
+                                    <?php echo  ${'type'.$x}['title_type'] ?>
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="subtitle">Land</div>
                                 <div class="title">
-                                    224,6 m&sup2;
+                                    <?php echo  ${'type'.$x}['land'] ?> m&sup2;
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="subtitle">Building</div>
                                 <div class="title">
-                                    190 m&sup2;
+                                    <?php echo  ${'type'.$x}['building'] ?> m&sup2;
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-lg-8">
+                        <div class="images-unit">
+                            <?php if(${'type'.$x}['images']['image_1']) { ?>
+                            <div class="item">
+                                <img class="w-100" src="<?php echo ${'type'.$x}['images']['image_1'] ?>" alt="">
+                            </div>
+                            <?php } ?>
+
+                            <?php if(${'type'.$x}['images']['image_2']) { ?>
+                            <div class="item">
+                                <img class="w-100" src="<?php echo ${'type'.$x}['images']['image_2'] ?>" alt="">
+                            </div>
+                            <?php } ?>
+                            <?php if(${'type'.$x}['images']['image_3']) { ?>
+                            <div class="item">
+                                <img class="w-100" src="<?php echo ${'type'.$x}['images']['image_3'] ?>" alt="">
+                            </div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
-                tabindex="0">
-                ...</div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"
-                tabindex="0">
-                ...</div>
+            <?php } ?>
         </div>
     </div>
 
 </div>
+
+<div class="testimony">
+    <div class="bg">
+        <img src="<?php bloginfo('template_directory');?>/images/unit/testimony-bg.png" alt="Background">
+    </div>
+    <div class="container-fluid">
+        <div class="items">
+            <div class="item-img">
+                <div class="ratio">
+                    <img class="w-100" src="<?php echo get_field('gallery')['image_1'] ?>" alt="">
+                </div>
+            </div>
+            <div class="item-img">
+                <div class="ratio">
+                    <img class="w-100" src="<?php echo get_field('gallery')['image_2'] ?>" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="title">
+                    What They Say About BRAVA
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="item">
+                    <div class="img">
+                        <img src="<?php echo get_field('testimony_1')['image'] ?>" alt="">
+                    </div>
+                    <div class="text">
+                        <?php echo get_field('testimony_1')['testimony_text'] ?>
+                    </div>
+                    <div class="name">
+                        <?php echo get_field('testimony_1')['name'] ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="item">
+                    <div class="img">
+                        <img src="<?php echo get_field('testimony_2')['image'] ?>" alt="">
+                    </div>
+                    <div class="text">
+                        <?php echo get_field('testimony_2')['testimony_text'] ?>
+                    </div>
+                    <div class="name">
+                        <?php echo get_field('testimony_2')['name'] ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
+        $('.images-unit').slick({
+            infinite: true,
+        });
+
+        $('.items').slick();
+    });
+
+    $('button[data-bs-toggle="pill"]').on('shown.bs.tab', function (e) {
+        $('.images-unit').slick('setPosition');
+    })
+</script>
 <?php
 get_footer();
